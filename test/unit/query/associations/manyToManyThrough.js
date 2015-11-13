42d30317ece8f9b0c59dfd80bd452d9155ead7e3
Alexandre Tiertant <a.tiertant@atlantis-software.net>
2015-11-13 15:43:16 +0100

    correct bug in manyToManyThrough

--- test/unit/query/associations/manyToManyThrough.js
+++ test/unit/query/associations/manyToManyThrough.js
@@ -1,69 +1,80 @@
-var Offshore = require('../../../../lib/offshore'),
-    assert = require('assert'),
-    async = require('async');
+var Offshore = require('../../../../lib/offshore');
+var assert = require('assert');
+var async = require('async');
 
-describe('Collection Query', function() {
+describe('Collection Query', function () {
 
-  describe('many to many through association', function() {
-    var Drive;
+  describe('many to many through association', function () {
+    var Driver;
+    var Ride;
+    var Taxi;
 
-    before(function(done) {
+    before(function (done) {
 
       var offshore = new Offshore();
       var collections = {};
 
-      collections.user = Offshore.Collection.extend({
-        identity: 'user',
+      collections.driver = Offshore.Collection.extend({
+        identity: 'Driver',
         connection: 'foo',
+        tableName: 'driver_table',
         attributes: {
-          id: {
-            type: 'integer',
-            primaryKey: true
+          "driverId": {
+            "type": "integer",
+            "primaryKey": true
           },
-          cars: {
-            collection: 'car',
-            through: 'drive',
-            via: 'car'
+          "driverName": {
+            "type": "string"
+          },
+          "taxis": {
+            "collection": "Taxi",
+            "via": "driver",
+            "through": "ride"
           }
         }
       });
 
-      collections.drive = Offshore.Collection.extend({
-        identity: 'drive',
+      collections.taxi = Offshore.Collection.extend({
+        identity: 'Taxi',
         connection: 'foo',
+        tableName: 'taxi_table',
         attributes: {
-          id: {
-            type: 'integer',
-            primaryKey: true
+          "taxiId": {
+            "type": "integer",
+            "primaryKey": true
           },
-          car: {
-            model: 'car'
+          "taxiMatricule": {
+            "type": "string"
           },
-          user: {
-            model: 'user'
+          "drivers": {
+            "collection": "Driver",
+            "via": "taxi",
+            "through": "ride"
           }
         }
       });
 
-      collections.car = Offshore.Collection.extend({
-        identity: 'car',
+      collections.ride = Offshore.Collection.extend({
+        identity: 'Ride',
         connection: 'foo',
+        tableName: 'ride_table',
         attributes: {
-          id: {
-            type: 'integer',
-            primaryKey: true
+          "rideId": {
+            "type": "integer",
+            "primaryKey": true
           },
-          drivers: {
-            collection: 'user',
-            via: 'cars',
-            dominant: true
+          "taxi": {
+            "model": "Taxi"
+          },
+          "driver": {
+            "model": "Driver"
           }
         }
       });
 
-      offshore.loadCollection(collections.user);
-      offshore.loadCollection(collections.drive);
-      offshore.loadCollection(collections.car);
+      offshore.loadCollection(collections.driver);
+      offshore.loadCollection(collections.taxi);
+      offshore.loadCollection(collections.ride);
 
       var connections = {
         'foo': {
@@ -71,36 +82,66 @@ describe('Collection Query', function() {
         }
       };
 
-      offshore.initialize({adapters: {adapter: require('offshore-memory')}, connections: connections }, function(err, colls) {
-        if(err) done(err);
-        User = colls.collections.user;
-        Drive = colls.collections.drive;
-        Car = colls.collections.car;
+      offshore.initialize({adapters: {adapter: require('offshore-memory')}, connections: connections}, function (err, colls) {
+        if (err)
+          done(err);
+        Driver = colls.collections.driver;
+        Taxi = colls.collections.taxi;
+        Ride = colls.collections.ride;
+
+        var drivers = [
+          {driverId: 1, driverName: 'driver 1'},
+          {driverId: 2, driverName: 'driver 2'}
+        ];
+        var taxis = [
+          {taxiId: 1, taxiMatricule: 'taxi_1'},
+          {taxiId: 2, taxiMatricule: 'taxi_2'}
+        ];
+        var rides = [
+          {rideId: 1, taxi: 1, driver: 1},
+          {rideId: 4, taxi: 2, driver: 2},
+          {rideId: 5, taxi: 1, driver: 2}
+        ];
+
         async.series([
           function (callback) {
-            User.create({id: 1}, callback);
+            Driver.createEach(drivers, callback);
           },
           function (callback) {
-            Drive.create({id: 1, car: 1, user: 1}, callback);
+            Taxi.createEach(taxis, callback);
           },
           function (callback) {
-            Car.create({id: 1}, callback);
+            Ride.createEach(rides, callback);
           }
-        ],function(err) {
+        ], function (err) {
           done();
         });
       });
     });
 
 
-    it('through table model associations should return a single objet', function(done) {
-      Drive.findOne(1)
-      .populate('car')
-      .populate('user')
-      .exec(function(err, drive) {
-        if(err) return done(err);
-	assert(!Array.isArray(drive.car),"through table model associations return Array instead of single Objet");
-        assert(!Array.isArray(drive.user),"through table model associations return Array instead of single Objet");
+    it('through table model associations should return a single objet', function (done) {
+      Ride.findOne(1)
+              .populate('taxi')
+              .populate('driver')
+              .exec(function (err, ride) {
+                if (err)
+                  return done(err);
+                assert(!Array.isArray(ride.taxi), "through table model associations return Array instead of single Objet");
+                assert(!Array.isArray(ride.driver), "through table model associations return Array instead of single Objet");
+                assert(ride.taxi.taxiId === 1);
+                assert(ride.taxi.taxiMatricule === 'taxi_1');
+                assert(ride.driver.driverId === 1);
+                assert(ride.driver.driverName === 'driver 1');
+                done();
+              });
+    });
+
+    it('through association shoud return many results', function (done) {
+      Driver.findOne(2).populate('taxis', {sort: {taxiId: 1}}).exec(function (err, driver) {
+        assert(driver.taxis.length === 2);
+        assert(driver.taxis[0].taxiId === 1);
+        assert(driver.taxis[0].taxiMatricule === 'taxi_1');
         done();
       });
     });
