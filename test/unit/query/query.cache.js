5c14b3958cecdb7d4dfb99abf2f171c3ef658102
Alexandre Tiertant <a.tiertant@atlantis-software.net>
2016-03-15 15:36:50 +0100

    add ability to use configurable adapter for cache persistance

--- test/unit/query/query.cache.js
+++ test/unit/query/query.cache.js
@@ -1,82 +1,163 @@
-var Offshore = require('../../../lib/offshore'),
-    assert = require('assert');
+var Offshore = require('../../../lib/offshore');
+var assert = require('assert');
 
 describe('Collection Query', function() {
 
   describe('.cache()', function() {
-    var query;
 
-    before(function(done) {
+    describe('default filesystem cache', function() {
+      var query;
 
-      var offshore = new Offshore();
-      var Model = Offshore.Collection.extend({
-        identity: 'random',
-        connection: 'foo',
-        attributes: {
-          number: {
-            type: 'integer',
-            defaultsTo: 1
-          },
-          doSomething: function() {}
-        }
-      });
+      before(function(done) {
 
-      offshore.loadCollection(Model);
+        var offshore = new Offshore();
+        var Model = Offshore.Collection.extend({
+          identity: 'random',
+          connection: 'foo',
+          attributes: {
+            number: {
+              type: 'integer',
+              defaultsTo: 1
+            },
+            doSomething: function() {
+            }
+          }
+        });
 
-      // Fixture Adapter Def
-      var adapterDef = { 
-        find: function(con, col, criteria, cb) {
-          return cb(null, [{number: Math.random() * 10000000000000000}]);
-        }
-      };
+        offshore.loadCollection(Model);
 
-      var connections = {
-        'foo': {
-          adapter: 'foobar'
-        }
-      };
+        // Fixture Adapter Def
+        var adapterDef = {
+          find: function(con, col, criteria, cb) {
+            return cb(null, [{number: Math.random() * 10000000000000000}]);
+          }
+        };
 
-      offshore.initialize({ adapters: { foobar: adapterDef }, connections: connections, cache: { prefix: 'test_cache_', defaultCacheTime: 100 } }, function(err, colls) {
-        if(err) return done(err);
-        query = colls.collections.random;
-        done();
-      });
-    });
+        var connections = {
+          'foo': {
+            adapter: 'foobar'
+          }
+        };
 
-    it('should cache results using user defined cacheKey and cacheTime', function(done) {
-      query.find({}).cache('cache_test', 10, function(err, first_values) {
-        assert(!err);
-        query.find({}).cache('cache_test', 10, function(err, cached_values) {
-          assert(!err);
-          assert(first_values[0].number === cached_values[0].number);
+        offshore.initialize({adapters: {foobar: adapterDef}, connections: connections, cache: {prefix: 'test_cache_', defaultCacheTime: 100}}, function(err, colls) {
+          if (err)
+            return done(err);
+          query = colls.collections.random;
           done();
         });
       });
-    });
 
-    it('should cache results using user defined cacheTime and automatic cacheKey', function(done) {
-      var criteria = { number: [1,2] };
-      query.find(criteria).cache(10, function(err, first_values) {
-        assert(!err);
-        query.find(criteria).cache(10, function(err, cached_values) {
-          assert(!err);
-          assert(first_values[0].number === cached_values[0].number);
-          done();
+      it('should cache results using user defined cacheKey and cacheTime', function(done) {
+        query.find({}).cache('cache_test', 10, function(err, first_values) {
+          assert(! err);
+          query.find({}).cache('cache_test', 10, function(err, cached_values) {
+            assert(! err);
+            assert(first_values[0].number === cached_values[0].number);
+            done();
+          });
+        });
+      });
+
+      it('should cache results using user defined cacheTime and automatic cacheKey', function(done) {
+        var criteria = {number: [1, 2]};
+        query.find(criteria).cache(10, function(err, first_values) {
+          assert(! err);
+          query.find(criteria).cache(10, function(err, cached_values) {
+            assert(! err);
+            assert(first_values[0].number === cached_values[0].number);
+            done();
+          });
+        });
+      });
+
+      it('should cache results with only a callback given', function(done) {
+        var criteria = {number: 100};
+        query.find(criteria).cache(function(err, first_values) {
+          assert(! err);
+          query.find(criteria).cache(function(err, cached_values) {
+            assert(! err);
+            assert(first_values[0].number === cached_values[0].number);
+            done();
+          });
         });
       });
     });
-    
-    it('should cache results with only a callback given', function(done) {
-      var criteria = { number: 100 };
-      query.find(criteria).cache(function(err, first_values) {
-        assert(!err);
-        query.find(criteria).cache(function(err, cached_values) {
-          assert(!err);
-          assert(first_values[0].number === cached_values[0].number);
+    describe('custom adapter cache', function() {
+      var query;
+
+      before(function(done) {
+
+        var offshore = new Offshore();
+        var Model = Offshore.Collection.extend({
+          identity: 'random',
+          connection: 'foo',
+          attributes: {
+            number: {
+              type: 'integer',
+              defaultsTo: 1
+            },
+            doSomething: function() {
+            }
+          }
+        });
+
+        offshore.loadCollection(Model);
+
+        // Fixture Adapter Def
+        var adapterDef = {
+          find: function(con, col, criteria, cb) {
+            return cb(null, [{number: Math.random() * 10000000000000000}]);
+          }
+        };
+
+        var connections = {
+          'foo': {
+            adapter: 'foobar'
+          }
+        };
+
+        offshore.initialize({adapters: {foobar: adapterDef}, connections: connections, cache: {prefix: 'test_cache_', defaultCacheTime: 100, adapter: require('offshore-memory')}}, function(err, colls) {
+          if (err)
+            return done(err);
+          query = colls.collections.random;
           done();
         });
       });
-    });
 
+      it('should cache results using user defined cacheKey and cacheTime', function(done) {
+        query.find({}).cache('cache_test', 10, function(err, first_values) {
+          assert(! err);
+          query.find({}).cache('cache_test', 10, function(err, cached_values) {
+            assert(! err);
+            assert(first_values[0].number === cached_values[0].number);
+            done();
+          });
+        });
+      });
+
+      it('should cache results using user defined cacheTime and automatic cacheKey', function(done) {
+        var criteria = {number: [1, 2]};
+        query.find(criteria).cache(10, function(err, first_values) {
+          assert(! err);
+          query.find(criteria).cache(10, function(err, cached_values) {
+            assert(! err);
+            assert(first_values[0].number === cached_values[0].number);
+            done();
+          });
+        });
+      });
+
+      it('should cache results with only a callback given', function(done) {
+        var criteria = {number: 100};
+        query.find(criteria).cache(function(err, first_values) {
+          assert(! err);
+          query.find(criteria).cache(function(err, cached_values) {
+            assert(! err);
+            assert(first_values[0].number === cached_values[0].number);
+            done();
+          });
+        });
+      });
+    });
   });
 });
